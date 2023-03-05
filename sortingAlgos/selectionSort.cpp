92d736a8d066171203aabbf5e0dbf3e0f2baae70
Vivek <codervivekk@gmail.com>
2023-03-05 22:59:32 +0530

    Written Swap Fxn

--- sortingAlgos/selectionSort.cpp
+++ sortingAlgos/selectionSort.cpp
@@ -5,6 +5,14 @@ using namespace std;
 where the first array is sorted and 2nd is unsorted,as the traversal moves ahead,we find the least
 element and inserts it to the beginning of the 1st array*/
 
+void swapp(int &a,int &b){
+    if(a!=b){
+        a=a+b;
+        b=a-b;
+        a=a-b;
+    }
+}
+
 void selectionSort(vector<int> &v){
     int size=v.size();
     for(int i=0;i<size-1;i++){
@@ -14,13 +22,14 @@ void selectionSort(vector<int> &v){
                 minIdx=j;
             }
         }
-        swap(v[minIdx],v[i]);
+        swapp(v[minIdx],v[i]);
     }
 }
 
 int main(){
     vector<int> v{23,5,32,6,54,94,2,5,3,64,4};
     selectionSort(v);
+
     for(int i=0;i<v.size();i++){
         cout<<v[i]<<" ";
     }
